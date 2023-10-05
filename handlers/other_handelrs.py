05e2b2d9ba670fbad5c3029f7ceff84e38041cf0
wxksy <azooro.manager@gmail.com>
2023-10-05 16:29:46 +0300

    Дописанный бот

---
+++ handlers/other_handelrs.py
@@ -0,0 +1,12 @@
+from aiogram.types import Message
+from lexicon.lexicon import LEXICON_RU
+from aiogram import Router
+
+router = Router()
+
+@router.message()
+async def send_echo(message:Message):
+    try:
+        await message.answer(text=message.text)
+    except TypeError:
+        await message.reply(text=LEXICON_RU['no_echo'])
